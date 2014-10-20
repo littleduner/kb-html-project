4d7e231fdbfe32001d9dd72ca9745d7ddf83c154
yansm <ysm12138@163.com>
2014-10-20 20:36:20 +0800

    增加确认医生点选时间，增加预约时间样式，看哥一遍成！

--- src/kb_afservice.js
+++ src/kb_afservice.js
@@ -1,4 +1,4 @@
-var doctor_info = [
+﻿var doctor_info = [
 	{
 		'name' : '闫三木'
 		,'keshi' : '内脏科'
@@ -58,47 +58,3 @@ $(function () {
 	})
 });
 
-
-$(function () {
-	var $active = $('header .active'),$headerLine = $('.header_line'),$headerLis = $('header li');
-	$headerLine.css('left', $active.index() * 54 + 'px');
-	$headerLis.hover(function () {
-		var $this = $(this),index = $this.index();
-		$headerLine.css('left', $this.index() * 54 + 'px');
-	},function () {
-		$headerLine.css('left', $active.index() * 54 + 'px');
-	});
-});
-var $userinfo;
-var $infocenter ;
-var $healthinfo;
-$(function () {
-	$userinfo = {
-	'photo':'http://www.baidu.com'
-	,'name':'闫三木'
-	,'age':'18'
-	,'sex':'man'
-	,'telphone':'18611572332'
-	,'address':'北京市清华大学紫荆公寓'
-	,'IDnum':'2342432434324'
-	};
-	/* 消息中心部分的内容 */
-	$infocenter = [
-		["在2014年10月30日有一个王医生的预约"],
-		["非常抱歉，您编号为20132的订单被取消了，请您注意查收退款"]
-	]
-	
-	$healthinfo={
-	'height':'175cm'
-	,'weight':'65kg'
-	,'familyhistory':'无'
-	}
-	
-	var infocenterul = $('.infocenter').find('ul');
-	for(var i=0;i<$infocenter.length;i++){
-		infocenterul.append("<li>"+$infocenter[i]+"</li>");
-	}
-	
-	
-	
-});
