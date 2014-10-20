130adcfd5ea5d54f079037c6f01ad771c6371e10
lvcheng1031 <425535361@qq.com>
2014-10-20 14:06:49 +0800

    修改了我的康保页面
    修改了我的康保页面，里面的样式还存在问题，数据不是真实的数据

--- src/my_kb.js
+++ src/my_kb.js
@@ -1,29 +1,33 @@
-﻿$(function () {
-	var kb_profile = {
+﻿var $userinfo;
+var $infocenter ;
+var $healthinfo;
+$(function () {
+	$userinfo = {
 	'photo':'http://www.baidu.com'
-	,'name':'lvcheng'
+	,'name':'闫三木'
 	,'age':'18'
-	,'sex':'男'
+	,'sex':'man'
+	,'telphone':'18611572332'
+	,'address':'北京市清华大学紫荆公寓'
+	,'IDnum':'2342432434324'
 	};
+	/* 消息中心部分的内容 */
+	$infocenter = [
+		["在2014年10月30日有一个王医生的预约"],
+		["非常抱歉，您编号为20132的订单被取消了，请您注意查收退款"]
+	]
+	
+	$healthinfo={
+	'height':'175cm'
+	,'weight':'65kg'
+	,'familyhistory':'无'
+	}
+	
+	var infocenterul = $('.infocenter').find('ul');
+	for(var i=0;i<$infocenter.length;i++){
+		infocenterul.append("<li>"+$infocenter[i]+"</li>");
+	}
 	
 	
-
-	$(function(){
-    $('button').click(function(){
-	  var url = $("input[type='text']").val(); 
-			 $.ajax({
-				 type: "GET",
-				 url: url,
-				 dataType: "json",
-				 success: function(data){
-						console.log(data);
-						  },
-				  error:function(XMLHttpRequest,errorinfo){
-						alert("连接失败~");
-				  }
-						  
-			 });
-		});
-	});
 	
 });
