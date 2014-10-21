597b72ba99f86c8ca47e8b603588c2960f427469
lvcheng1031 <425535361@qq.com>
2014-10-21 13:31:02 +0800

    在myJquery中增加了cookie的一些代码，增加，删除，获取
    没有测试，一会儿有会，今天又到这了

--- src/myJquery.js
+++ src/myJquery.js
@@ -8,3 +8,27 @@
 		$headerLine.css('left', $active.index() * 54 + 'px');
 	});
 });
+function addCookie(name,value,expiresHours){ 
+var cookieString=name+"="+escape(value); 
+//判断是否设置过期时间 
+if(expiresHours>0){ 
+var date=new Date(); 
+date.setTime(date.getTime+expiresHours*3600*1000); 
+cookieString=cookieString+"; expires="+date.toGMTString(); 
+} 
+document.cookie=cookieString; 
+} 
+function getCookie(name){ 
+var strCookie=document.cookie; 
+var arrCookie=strCookie.split("; "); 
+for(var i=0;i<arrCookie.length;i++){ 
+var arr=arrCookie[i].split("="); 
+if(arr[0]==name)return arr[1]; 
+} 
+return ""; 
+} 
+function deleteCookie(name){ 
+var date=new Date(); 
+date.setTime(date.getTime()-10000); 
+document.cookie=name+"=v; expires="+date.toGMTString(); 
+} 
